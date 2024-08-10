aa65b3899b5fde11b1d6ca41b09483f279aaf3e1
Ruixin Zhang <ruixin.zhang@duke.edu>
2024-08-10 01:00:33 -0400

    Moved all the tools outside their parent folder, also summarizer pending

--- benchmark/chameleon/run/tools/tabtools.py
+++ benchmark/chameleon/run/tools/tabtools.py
@@ -14,6 +14,11 @@ from torch.utils.data import DataLoader
 # Hugging Face datasets
 from datasets import Dataset, DatasetDict, concatenate_datasets
 
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+
 # Good old Transformer models
 from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
 from transformers import PreTrainedTokenizerFast, BatchEncoding
@@ -31,6 +36,36 @@ from tokenizers.trainers import WordLevelTrainer
 
 # Confusion matrix
 from sklearn.metrics import confusion_matrix
+
+import time
+import logging
+import os
+import sys
+from dataclasses import dataclass, field
+from typing import Optional
+import pandas as pd
+from pathlib import Path
+
+import nltk  # Here to have a nice missing dependency error message early on
+import numpy as np
+from datasets import load_metric
+from datasets import Dataset, DatasetDict
+
+import transformers
+from filelock import FileLock
+from transformers import (
+    AutoConfig,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    DataCollatorForSeq2Seq,
+    HfArgumentParser,
+    Seq2SeqTrainer,
+    Seq2SeqTrainingArguments,
+    set_seed,
+)
+from transformers import AutoTokenizer
+from transformers.trainer_utils import get_last_checkpoint, is_main_process
+from transformers.utils import check_min_version
 
 # Fixing the random seeds
 RANDOM_SEED = 1729
@@ -262,6 +297,29 @@ class table_toolkits():
         except Exception as e:
             return "Error: "+str(e)
         # other exceptions
+    
+    def numerical_classifier(self, model_name, feature, target):
+        scaler = MinMaxScaler()
+        df_train = self.dataset_dict["train"].to_pandas()
+        df_test = self.dataset_dict["test"].to_pandas()
+        X_train = df_train[[feature]]
+        X_test = df_test[[feature]]
+        X_train_scaled = scaler.fit_transform(X_train)
+        X_test_scaled = scaler.fit_transform(X_test)
+        y_train = self.dataset_dict["train"].to_pandas()[target]
+        if model_name=="random_forest":
+            rf_model = RandomForestClassifier(n_estimators=100, random_state=42)
+            rf_model.fit(X_train_scaled, y_train)
+            probabilities = rf_model.predict_proba(X_test_scaled)[:, 1]
+        elif model_name=="mlp":
+            nn_model = MLPClassifier(hidden_layer_sizes=(10,), max_iter=500, random_state=42)
+            nn_model.fit(X_train_scaled, y_train)
+            probabilities = nn_model.predict_proba(X_test_scaled)[:, 1]
+        elif model_name=="svm":
+            svm_model = SVC(kernel='linear', probability=True, random_state=42)
+            svm_model.fit(X_train_scaled, y_train)
+            probabilities = svm_model.predict_proba(X_test_scaled)[:, 1]
+        return probabilities
             
     def textual_classifier(self, model_name, section, target, one_v_all="None"):
         """
@@ -382,8 +440,8 @@ class table_toolkits():
                 if model_name != 'naive_bayes': # CHANGE 'naive_bayes' (shannon)
                     tokenizer.model_max_length = max_length
                     tokenizer.max_length = max_length
-                tokenizer.save("/usr/project/xtmp/rz95/InterpretableQA-LLMTools/benchmark/chameleon/run/tools/table/temp_tokenizer.json")  # <YOUR_OWN_PATH>
-                tokenizer = PreTrainedTokenizerFast(tokenizer_file="/usr/project/xtmp/rz95/InterpretableQA-LLMTools/benchmark/chameleon/run/tools/table/temp_tokenizer.json") # <YOUR_OWN_PATH>
+                tokenizer.save("/usr/project/xtmp/rz95/InterpretableQA-LLMTools/benchmark/chameleon/run/tools/temp/temp_tokenizer.json")  # <YOUR_OWN_PATH>
+                tokenizer = PreTrainedTokenizerFast(tokenizer_file="/usr/project/xtmp/rz95/InterpretableQA-LLMTools/benchmark/chameleon/run/tools/temp/temp_tokenizer.json") # <YOUR_OWN_PATH>
 
                 if model_name != 'naive_bayes': # CHANGE 'naive_bayes'
                     tokenizer.model_max_length = max_length
@@ -649,14 +707,337 @@ class table_toolkits():
         predictions_to_categories = [unique_classes[x] for x in predictions]
         return {"predictions": predictions_to_categories}
 
+    def summarizer(self, section, target):
+        check_min_version("4.5.0.dev0")
+        local_rank = int(os.environ.get('LOCAL_RANK', 0))
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+        logger = logging.getLogger(__name__)
+        nltk.data.path.append("/usr/project/xtmp/rz95/")
+
+        try:
+            nltk.data.find("tokenizers/punkt")
+            print("Found nltk")
+        except (LookupError, OSError):
+            with FileLock(".lock") as lock:
+                nltk.download("punkt", download_dir='/usr/project/xtmp/rz95/', quiet=True)
+                print("Finished downloading punkt")
+        
+        last_checkpoint = None
+        output_dir = "outputs/summarization-t5-small"
+        if os.path.isdir(output_dir):
+            last_checkpoint = get_last_checkpoint(output_dir)
+        
+        cache_dir = "/usr/project/xtmp/rz95/.cache/huggingface/" #<YOUR_OWN_PATH>
+
+        # Setup logging
+        logging.basicConfig(
+            format="%(asctime)s - %(levelname)s - %(name)s -   %(message)s",
+            datefmt="%m/%d/%Y %H:%M:%S",
+            handlers=[logging.StreamHandler(sys.stdout)],
+        )
+        logger.setLevel(logging.INFO if is_main_process(local_rank) else logging.WARN)
+
+        # Log on each process the small summary:
+        logger.warning(
+            f"Process rank: {local_rank}, device: {device}"
+            + f"distributed training: {bool(local_rank != -1)}"
+        )
+        # Set the verbosity to info of the Transformers logger (on main process only):
+        if is_main_process(local_rank):
+            transformers.utils.logging.set_verbosity_info()
+
+        # Set seed before initializing model.
+        set_seed(RANDOM_SEED)
+        model_name = "t5-small"
+        
+        parser = HfArgumentParser((Seq2SeqTrainingArguments))
+        if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+            # If we pass only one argument to the script and it's the path to a json file,
+            # let's parse it to get our arguments.
+            _, _, training_args = parser.parse_json_file(json_file=os.path.abspath(sys.argv[1]))
+        else:
+            _, _, training_args = parser.parse_args_into_dataclasses()
+        
+        def _preprocess_dataset_for_summarization(dataset, sep_token):
+            # For filtering out continued patents from our dataset
+            decision_to_str = {
+                'REJECTED': 0,
+                'ACCEPTED': 1,
+                'PENDING': 2,
+                'CONT-REJECTED': 3,
+                'CONT-ACCEPTED': 4,
+                'CONT-PENDING': 5
+            }
+
+            # Indices of cont patents
+            indices_of_cont_patents = {v for k, v in decision_to_str.items() if k.startswith('CONT-')}
+
+            def map_decision_to_string(example):
+                return {'decision': decision_to_str[example['decision']]}
+
+            dataset = dataset.map(map_decision_to_string)
+
+            def filter_cont_patents(e):
+                return e['decision'] not in indices_of_cont_patents
+
+            def format_example_for_summarization(e):
+                if 'What is claimed is:' in e['claims'][:50]:
+                    e['claims'] = e['claims'].replace('What is claimed is:', '')
+
+                # Format
+                # NOTE: The tokenizer will add `bos` and `eos` tokens automatically, so we do not add them here
+                text = 'TITLE {title} {sep} CLAIMS {claims}'.format(
+                    sep=sep_token, title=e['title'], claims=e['claims']
+                )
+                return {
+                    'claims_for_summarization': text,
+                    'abstract_for_summarization': e['abstract']
+                }
+
+            dataset = dataset.filter(filter_cont_patents)
+            # Format examples
+            dataset = dataset.map(format_example_for_summarization, batched=False)
+            return dataset
+
+        def preprocess_dataset_for_summarization(dataset_dict, tokenizer):
+            """ Loads dataset for language modeling. Note that the tokenizer is needed in order 
+                to add [CLS] and [BOS] tokens to the data. """
+
+            start_time = time.time()
+            # Add new tokens to the tokenizer
+            new_tokens = ['TITLE', 'CLAIMS']
+            tokenizer.add_tokens(new_tokens)
+            if not bool(tokenizer.sep_token):  # we need to add a <sep> token
+                tokenizer.sep_token = '<sep>'
+
+            # Create training and validation datasets
+            print('>>> Training dataset')
+            dataset_dict["train"] = _preprocess_dataset_for_summarization(
+                dataset_dict["train"],
+                sep_token=tokenizer.special_tokens_map['sep_token']
+            )
+            print('>>> Validation dataset')
+            dataset_dict["validation"] = _preprocess_dataset_for_summarization(
+                dataset_dict["validation"], 
+                sep_token=tokenizer.special_tokens_map['sep_token']
+            )
+            
+            print(f'****************** Finished loading dataset in {time.time() - start_time:.1f} seconds ******************')
+            return dataset_dict, tokenizer
+
+        # Load pretrained model and tokenizer
+        
+        # Distributed training:
+        # The .from_pretrained methods guarantee that only one local process can concurrently
+        # download model & vocab.
+        config = AutoConfig.from_pretrained(
+            model_name,
+            cache_dir=cache_dir,
+            revision="main",
+        )
+        tokenizer = AutoTokenizer.from_pretrained(
+            model_name,
+            cache_dir=cache_dir,
+            use_fast=True,
+            revision="main",
+        )
+
+        print('Loading model from pretrained checkpoint')
+        model = AutoModelForSeq2SeqLM.from_pretrained(
+            model_name,
+            from_tf=bool(".ckpt" in model_name),
+            config=config,
+            cache_dir=cache_dir,
+            revision="main",
+        )
+        
+        train_df = []
+        path = "/usr/project/xtmp/rz95/InterpretableQA-LLMTools"
+        for sub in range(2004, 2006):
+            train_df.append(pd.read_csv("{}/data/external_corpus/hupd/hupd_{}.csv".format(path, sub)))
+        train_df = pd.concat(train_df, ignore_index=True)
+        val_df = pd.read_csv("{}/data/external_corpus/hupd/hupd_{}.csv".format(path, 2007))
+        test_df = pd.read_csv("{}/data/external_corpus/hupd/hupd_{}.csv".format(path, 2008))
+        train_dataset = Dataset.from_pandas(train_df)
+        val_dataset = Dataset.from_pandas(val_df)
+        test_dataset = Dataset.from_pandas(test_df)
+        datasets = DatasetDict({"train": train_dataset, "validation": val_dataset, "test": test_dataset})
+
+        # Preprocess dataset
+        datasets, tokenizer = preprocess_dataset_for_summarization(datasets, tokenizer)
+        
+        # Resize model embeddings for tokenizer
+        model.resize_token_embeddings(len(tokenizer))
+        
+        # Prediction
+        # print("Using validation dataset for prediction")
+        datasets["test"] = datasets["validation"]
+
+        if model.config.decoder_start_token_id is None:
+            raise ValueError("Make sure that `config.decoder_start_token_id` is correctly defined")
+
+        prefix = "summarize: "
+
+        # Preprocessing the datasets.
+        # We need to tokenize inputs and targets.
+        column_names = datasets["train"].column_names
+
+        # Get the column names for input/target.
+        dataset_columns = ("claims_for_summarization", "abstract_for_summarization")
+        text_column = section
+        if text_column not in column_names:
+            raise ValueError(
+                f"--text_column' value '{section}' needs to be one of: {', '.join(column_names)}"
+            )
+        summary_column = target
+        if summary_column not in column_names:
+            raise ValueError(
+                f"--summary_column' value '{target}' needs to be one of: {', '.join(column_names)}"
+            )
+
+        # Temporarily set max_target_length for training.
+        max_target_length = 128 
+        padding = "max_length" 
+
+        def preprocess_function(examples):
+            inputs = examples[text_column]
+            targets = examples[summary_column]
+            inputs = [prefix + inp for inp in inputs]
+            model_inputs = tokenizer(inputs, max_length=1024, padding=padding, truncation=True)
+
+            # Setup the tokenizer for targets
+            with tokenizer.as_target_tokenizer():
+                labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
+
+            # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+            # padding in the loss.
+            if padding == "max_length":
+                labels["input_ids"] = [
+                    [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+                ]
+
+            model_inputs["labels"] = labels["input_ids"]
+            return model_inputs
+
+        train_dataset = datasets["train"]
+        if "train" not in datasets:
+            raise ValueError("--do_train requires a train dataset")
+        if "test" not in datasets:
+            raise ValueError("--do_predict requires a test dataset")
+        test_dataset = datasets["test"]
+        test_dataset = test_dataset.map(
+            preprocess_function,
+            batched=True,
+            remove_columns=column_names,
+            load_from_cache_file=True,
+        )
+
+        # Data collator
+        label_pad_token_id = -100 
+        data_collator = DataCollatorForSeq2Seq(
+            tokenizer,
+            model=model,
+            label_pad_token_id=label_pad_token_id,
+            pad_to_multiple_of=None,
+        )
+
+        metric = load_metric("rouge")
+
+        def postprocess_text(preds, labels):
+            preds = [pred.strip() for pred in preds]
+            labels = [label.strip() for label in labels]
+
+            # rougeLSum expects newline after each sentence
+            preds = ["\n".join(nltk.sent_tokenize(pred)) for pred in preds]
+            labels = ["\n".join(nltk.sent_tokenize(label)) for label in labels]
+
+            return preds, labels
+
+        def compute_metrics(eval_preds):
+            preds, labels = eval_preds
+            if isinstance(preds, tuple):
+                preds = preds[0]
+            decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+            # Replace -100 in the labels as we can't decode them.
+            labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+            decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+            # Some simple post-processing
+            decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+            result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+            # Extract a few results from ROUGE
+            result = {key: value.mid.fmeasure * 100 for key, value in result.items()}
+
+            prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+            result["gen_len"] = np.mean(prediction_lens)
+            result = {k: round(v, 4) for k, v in result.items()}
+            return result
+
+        # Initialize our Trainer
+        trainer = Seq2SeqTrainer(
+            model=model,
+            train_dataset=train_dataset,
+            eval_dataset=None, #eval_dataset,
+            tokenizer=tokenizer,
+            data_collator=data_collator,
+            compute_metrics=compute_metrics,
+        )
+
+        # Training
+        if last_checkpoint is not None:
+            checkpoint = last_checkpoint
+        elif os.path.isdir(model_name):
+            checkpoint = model_name
+        else:
+            checkpoint = None
+        train_result = trainer.train(resume_from_checkpoint=checkpoint)
+        trainer.save_model()  # Saves the tokenizer too for easy upload
+
+        metrics = train_result.metrics
+        max_train_samples = (
+            len(train_dataset)
+        )
+        metrics["train_samples"] = min(max_train_samples, len(train_dataset))
+
+        trainer.log_metrics("train", metrics)
+        trainer.save_metrics("train", metrics)
+        trainer.save_state()
+
+        # Evaluation
+        results = {}
+        logger.info("*** Test ***")
+        test_results = trainer.predict(
+            test_dataset,
+            metric_key_prefix="test"
+        )
+        metrics = test_results.metrics
+        metrics["test_samples"] = len(test_dataset)
+        trainer.log_metrics("test", metrics)
+        trainer.save_metrics("test", metrics)
+
+        if trainer.is_world_process_zero():
+            test_preds = tokenizer.batch_decode(
+                test_results.predictions, skip_special_tokens=True, clean_up_tokenization_spaces=True
+            )
+            test_preds = [pred.strip() for pred in test_preds]
+            output_test_preds_file = os.path.join(output_dir, "test_generations.txt")
+            with open(output_test_preds_file, "w") as writer:
+                writer.write("\n".join(test_preds))
+
+        return test_preds
+
 if __name__ == "__main__":
     db = table_toolkits()
-    db.db_loader('hupd', '2016-2016', 'None', 'None')
+    # db.db_loader('hupd', '2016-2016', 'None', 'None')
     # pandas_code = "import pandas as pd\ndf['filing_month'] = df['filing_date'].apply(lambda x:x.month)\nmonth = df['filing_month'].mode()[0]"
     # print(db.pandas_interpreter(pandas_code))
 
-    print(db.db_loader('hupd', '2004-2006', '2007-2007', 'decision'))
-    db.textual_classifier('cnn', 'summary', 'decision', 'ACCEPTED')
+    # print(db.db_loader('hupd', '2004-2006', '2007-2007', 'decision'))
+    # db.textual_classifier('cnn', 'summary', 'decision', 'ACCEPTED')
     # print(db.db_loader('neurips', '0-1000', '1001-3583', 'Topic'))
     # db.textual_classifier('cnn', 'Abstract', 'Topic', 'Deep Learning')
     # logistic_regression, distilbert-base-uncased, cnn, naive_bayes hupd: # title, abstract, summary, claims, background, full_description # decision    
+    
+    print(db.summarizer("t5-small", "abstract", "title"))
