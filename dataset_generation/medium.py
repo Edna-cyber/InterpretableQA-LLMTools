888211d72388d53bf22ab2f238c0a55b7a54f984
Ruixin Zhang <ruixin.zhang@duke.edu>
2024-08-25 11:26:51 -0400

    Modified medium questions to not involve training, debugged easy questions answer generation

--- dataset_generation/medium.py
+++ dataset_generation/medium.py
@@ -30,7 +30,7 @@ def predict_oral(num):
 
 question_id = 1
 question_type_count = {7:100, 8:100, 9:100, 10:100}
-question_types = [8] #[7,8,9,10]
+question_types = [7,8,9,10]
 with jsonlines.open('/usr/project/xtmp/rz95/InterpretableQA-LLMTools/data/questions/medium.jsonl', mode='w') as writer:
     while question_id<=10: # 400
         question_type = random.choice(question_types) 
@@ -49,17 +49,15 @@ with jsonlines.open('/usr/project/xtmp/rz95/InterpretableQA-LLMTools/data/questi
                     question_types.remove(7)
                 question_id += 1
         elif question_type==8:
-            # Using patent applications from {start_year} to {end_year} for training, predict the decisions of applications from {year_not_in_the_range} with unique indices {indices_str}. Return a list of "ACCEPTED" and "REJECTED".
-            start_year = random.randint(2004,2012)
-            end_year = random.randint(start_year,2012)
+            # For a patent application with an abstract {abstract_content}, predict whether it will get accepted. Return either 'ACCEPTED' or 'REJECTED'.
             year_not_in_the_range = random.randint(2013,2018)
             df = pd.read_csv(os.path.join(corpus_dir, "hupd/hupd_{}.csv").format(year_not_in_the_range))
             indices_to_choose = df.index[(df['decision']=="ACCEPTED") | (df['decision']=="REJECTED")].to_list()
-            indices = random.sample(indices_to_choose,8)
-            indices_str = ",".join(["ID-"+str(x) for x in indices])
-            decisions = [df.at[index,"decision"] for index in indices]
-            question = "Using patent applications from {} to {} for training, predict the decisions of applications from {} with unique indices {}. Return a list of 'ACCEPTED' and 'REJECTED'.".format(start_year, end_year, year_not_in_the_range, indices_str)
-            answer = decisions
+            index = random.sample(indices_to_choose,1)
+            abstract_content = df.at[index,"abstract"]
+            decision = df.at[index,"decision"]
+            question = "For a patent application with an abstract {}, predict whether it will get accepted. Return either 'ACCEPTED' or 'REJECTED'.".format(abstract_content)
+            answer = decision
             if answer:
                 writer.write({"qid": "medium-{:0>4d}".format(question_id), "question_type":str(question_type), "question":question, "answer":str(answer)})
                 question_type_count[8] -= 1
@@ -67,23 +65,19 @@ with jsonlines.open('/usr/project/xtmp/rz95/InterpretableQA-LLMTools/data/questi
                     question_types.remove(8)
                 question_id += 1
         elif question_type==9:
-            # Using the rows 1-{num} of NeurIPS papers for training, predict whether the NeurIPS papers with unique indices {indices} belong to {topic}?
+            # For a NeurIPS 2023 paper with title {title_content}, predict whether it belongs to {topic}? Return either '{topic}' or 'not {topic}'.
             topic = random.choice(["Deep Learning", "Reinforcement Learning", "Health", "Applications", "Theory", "Data-centric AI", "Probabilistic Methods", "Social Aspects", "Optimization"]) 
-            row_num = random.choice(list(range(1000, 3000+1, 250)))
             df = pd.read_csv(os.path.join(corpus_dir, "neurips/NeurIPS_2023_Papers.csv"))
-            df = df.iloc[row_num+1:]
+            df = df.iloc[3001:]
             indices_to_choose = df.index[df["Topic"].notna()].to_list()
-            indices = random.sample(indices_to_choose,8)
-            indices_str = ",".join(["ID-"+str(x) for x in indices])
-            topics = []
-            for index in indices:
-                if topic in df.at[index,"Topic"]:
-                    topics.append(topic)
-                else:
-                    topics.append("not "+topic)
-            question_phrasings = ["Using the rows 1-{} of NeurIPS papers for training, predict whether the NeurIPS papers with unique indices {} belong to {}? Return a list of '{}' and 'not {}'.", "Given the rows 1-{} from NeurIPS papers for training, predict whether the NeurIPS papers with unique indices {} fall into the {} category? Return a list of '{}' and 'not {}'.", "Given the {} of NeurIPS papers, predict whether the NeurIPS papers with unique indices {} are anticipated to be classified under the {} topic? Return a list of '{}' and 'not {}'."] 
-            question = question_phrasings[random.randint(0,len(question_phrasings)-1)].format(row_num, indices_str, topic, topic, topic)
-            answer = topics
+            index = random.sample(indices_to_choose,1)
+            title_content = df.at[index,"Title"]
+            if topic in df.at[index,"Topic"]:
+                belong = topic
+            else:
+                belong = "not "+topic
+            question = "For a NeurIPS 2023 paper with title {}, predict whether it belongs to {}? Return either '{}' or 'not {}'.".format(title_content, topic, topic, topic)
+            answer = belong
             if answer:
                 writer.write({"qid": "medium-{:0>4d}".format(question_id), "question_type":str(question_type), "question":question, "answer":str(answer)})
                 question_type_count[9] -= 1
@@ -91,26 +85,22 @@ with jsonlines.open('/usr/project/xtmp/rz95/InterpretableQA-LLMTools/data/questi
                     question_types.remove(9)
                 question_id += 1
         else:
-            # Train the model with the first {num} NeurIPS papers and then predict whether the papers with unique indices {indices} will be accepted as oral presentations.
-            num = random.choice(list(range(1000,3500+1,100)))
+            # For a NeurIPS 2023 paper with abstract {abstract_content}, predict whether it will be accepted as an oral presentation? Return either ‘oral’ or ‘not oral’.
             df = pd.read_csv(os.path.join(corpus_dir, "neurips/NeurIPS_2023_Papers.csv"))
-            df = df.iloc[num+1:]
+            df = df.iloc[3001:]
             indices_to_choose = df.index[df["Oral"].notna()].to_list()
-            indices = random.sample(indices_to_choose,8)
-            indices_str = ",".join(["ID-"+str(x) for x in indices])
-            orals = []
-            for index in indices:
-                if df.at[index,"Oral"]:
-                    orals.append("Oral")
-                else:
-                    orals.append("not Oral")
-            question_phrasings = ["Train the model with the first {} papers and then predict whether the papers with unique indices {} will be accepted as oral presentations. Return a list of 'Oral' and 'not Oral'.", "Using the first {} papers as the training set, predict whether the papers with unique indices {} will be accepted as oral presentations or not. Return a list of 'Oral' and 'not Oral'.", "Train the model using the initial {} papers, and then predict whether the papers with unique indices {} will be selected for oral presentations. Return a list of 'Oral' and 'not Oral'."]
-            question = question_phrasings[random.randint(0,len(question_phrasings)-1)].format(num, indices)
-            answer = orals
+            index = random.sample(indices_to_choose,1)
+            abstract_content = df.at[index,"Abstract"]
+            if df.at[index,"Oral"]:
+                oral = "Oral"
+            else:
+                oral = "not Oral"
+            question = "For a NeurIPS 2023 paper with abstract {}, predict whether it will be accepted as an oral presentation? Return either ‘oral’ or ‘not oral’.".format(abstract_content)
+            answer = oral
             if answer:
                 writer.write({"qid": "hard-hupd-{:0>4d}".format(question_id), "question_type":str(question_type), "question":question, "answer":str(answer)})
-                question_type_count[14] -= 1
-                if question_type_count[14]==0:
-                    question_types.remove(14)
+                question_type_count[10] -= 1
+                if question_type_count[10]==0:
+                    question_types.remove(10)
                 question_id += 1
 
