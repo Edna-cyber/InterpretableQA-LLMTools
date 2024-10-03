808d61665a0ab7cf0b7c798eec40ea654d33261e
chengstark <stark.cheng@outlook.com>
2024-10-03 12:58:10 -0400

    fist

--- benchmark/chameleon/run/api/gemini.py
+++ benchmark/chameleon/run/api/gemini.py
@@ -1,36 +1,22 @@
-import google.generativeai as genai
-from google.generativeai.types import HarmCategory, HarmBlockThreshold
-import os 
-from PIL import Image
-import pdb
 import requests
-from time import sleep
-import json
+from vertexai.generative_models import (
+    FunctionDeclaration,
+    GenerationConfig,
+    GenerativeModel,
+    Part,
+    Tool,
+)
 
-GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
-print("Gemini key", GOOGLE_API_KEY)
-gemini_client = genai.configure(api_key=GOOGLE_API_KEY)
 
-safety_settings = {
-    HarmCategory.HARM_CATEGORY_HARASSMENT: HarmBlockThreshold.BLOCK_NONE,
-    HarmCategory.HARM_CATEGORY_HATE_SPEECH: HarmBlockThreshold.BLOCK_NONE,
-    HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT: HarmBlockThreshold.BLOCK_NONE,
-    HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT: HarmBlockThreshold.BLOCK_NONE,
-}
 
-def call_gemini_pro(model, prompt, temperature, max_tokens, tools, tool_choice):
-    response = gemini_client.generate_content(
-        model=model, 
-        prompt=prompt,
-        temperature=temperature,
-        max_tokens=args.policy_max_tokens, 
-        tools=max_tokens,
-        tool_choice=tool_choice 
+
+tools_gemini_converted = [
+    FunctionDeclaration(
+        name=x['function_declarations'][0]['name'],
+        description=x['function_declarations'][0]['description'],
+        parameters=x['function_declarations'][0]['parameters'] if 'parameters' in x['function_declarations'][0].keys() else None,
     )
-    print("response", response)
-    choice = response.choices[0]  
-    return choice
 
-if __name__ == '__main__':
-    call_gemini_pro(model, prompt, temperature, max_tokens, tools, tool_choice)
+    for x in tools_gemini
     
+]
--- benchmark/chameleon/run/scripts/run_easy_otherllm.sh
+++ benchmark/chameleon/run/scripts/run_easy_otherllm.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=easyotherllm
 #SBATCH -t 24:00:00
 #SBATCH --mem=100GB
-#SBATCH -p compsci-gpu
+#SBATCH -p compsci
 #SBATCH --cpus-per-task 8
 #SBATCH --output=./easyotherllm.log
 
@@ -12,7 +12,7 @@ export ANTHROPIC_API_KEY="..."
 eval "$(conda shell.bash hook)"
 conda activate nlp-env
 
-python ../run-v2-otherllm.py --label chameleon_chatgpt --policy_engine gpt-3.5-turbo --policy_temperature 0.0 --policy_max_tokens 1000 --hardness easy --prompt clean --formula formula1
-python ../run-v2-otherllm.py --label chameleon_chatgpt --policy_engine gpt-4-turbo --policy_temperature 0.0 --policy_max_tokens 1000 --hardness easy --prompt clean --formula formula1
+# python ../run-v2-otherllm.py --label chameleon_chatgpt --policy_engine gpt-3.5-turbo --policy_temperature 0.0 --policy_max_tokens 1000 --hardness easy --prompt clean --formula formula1
+# python ../run-v2-otherllm.py --label chameleon_chatgpt --policy_engine gpt-4-turbo --policy_temperature 0.0 --policy_max_tokens 1000 --hardness easy --prompt clean --formula formula1
 python ../run-v2-otherllm.py --label chameleon_chatgpt --policy_engine gemini --policy_temperature 0.0 --policy_max_tokens 1000 --hardness easy --prompt clean --formula formula1
-python ../run-v2-otherllm.py --label chameleon_chatgpt --policy_engine claude --policy_temperature 0.0 --policy_max_tokens 1000 --hardness easy --prompt clean --formula formula1
+# python ../run-v2-otherllm.py --label chameleon_chatgpt --policy_engine claude --policy_temperature 0.0 --policy_max_tokens 1000 --hardness easy --prompt clean --formula formula1
